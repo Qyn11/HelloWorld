47c44e098c06636d5b3dfbe17498531ffdfe9193
Qyn11 <quyen.tran11@outlook.de>
2021-07-31 21:07:18 +0200

    Hello world and Hello Quyen

--- src/Hello_World.java
+++ src/Hello_World.java
@@ -1,5 +1,6 @@
 public class Hello_World {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Hello World");
+        System.out.println("Hello Quyen");
     }
 }
